a1975c26c864dfbd832483a21f48875124604f51
Criss2k23 <129349000+Criss2k23@users.noreply.github.com>
2023-06-06 20:30:53 -0500

    Add files via upload

---
+++ break.continue.js
@@ -0,0 +1,13 @@
+let c = 0, mensaje = '';
+
+while (c < 10){
+    c++
+    if (c === 50){
+        //document.write("termina el bucle del 'BREAK' ")
+        //break;
+        document.write("Salta a la siguiente linea por 'CONTINUE' " + '<br>')
+    }
+    mensaje = `${c} <br>`
+    document = `${c} <br>`
+    document.write(mensaje);
+}
